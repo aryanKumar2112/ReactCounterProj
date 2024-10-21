66f961179f673438dc5986cc3f9de0173d46280f
Aryan <aryankumar211202@gmail.com>
2024-10-22 00:19:55 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  
+  const[count,setCount]=useState(0);
+  const clickHandler=()=>{
+    //setCount(count+1);
+    setCount(count=>count+1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>count :{count}</h1>
+      <button onClick={clickHandler}>Increase By 1</button>
+      
     </div>
   );
 }
